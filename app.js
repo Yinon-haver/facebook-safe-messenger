2f67db06dd242d4ecb1a6b32ff8d0a960451b7a5
yinonhever <yinon.hever@appsflyer.com>
2018-09-03 17:45:57 +0300

    test get started

--- app.js
+++ app.js
@@ -725,6 +725,31 @@ function callSendAPI(messageData) {
 		}
 	});
 }
+function greetUserText(userId) {
+    //first read user firstname
+    request({
+        uri: 'https://graph.facebook.com/v2.7/' + userId,
+        qs: {
+            access_token: config.FB_PAGE_TOKEN
+        }
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var user = JSON.parse(body);
+            if (user.first_name) {
+                console.log("FB user: %s %s, %s",
+                    user.first_name, user.last_name, user.gender);
+                sendTextMessage(userId, "Welcome " + user.first_name + '!');
+            } else {
+                console.log("Cannot get data for fb user with id",
+                    userId);
+            }
+        } else {
+            console.error(response.error);
+        }
+    });
+}
+
+
 
 
 
@@ -748,10 +773,10 @@ function receivedPostback(event) {
         case 'JOB_APPLY':
             sendToDialogFlow(senderID,"hey");
         case 'GET_STARTED':
-            sendToDialogFlow(senderID,"it is a test");
+            greetUserText(senderID);
 		default:
 			//unindentified payload
-			sendTextMessage(senderID, "I'm not sure what you want. Can you be more specific?");
+			sendTextMessage(senderID, "I'm not sure what you want. Can you be more yinon?");
 			break;
 
 	}
